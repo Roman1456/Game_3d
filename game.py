718c01b9e0b51334f474edb1ff15051fe9299f9d
Roman1456 <151393377+Roman1456@users.noreply.github.com>
2024-12-11 16:18:28 +0300

    game

---
+++ game.py
@@ -0,0 +1,10 @@
+from direct.showbase.ShowBase import ShowBase
+from mapmanager import Mapmanager
+
+class Game(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+        self.land = Mapmanager()
+        base.camlens.setFor(90)
+game = Game()
+game.run()
